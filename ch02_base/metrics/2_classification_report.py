3b1de5c9e4165310cedf53ec370f787c75cfd92e
maomin <1220826363@qq.com>
2026-05-24 22:20:55 +0800

    new: F1分数指标 + 评估报告

---
+++ ch02_base/metrics/2_classification_report.py
@@ -0,0 +1,24 @@
+from sklearn.metrics import classification_report
+from sklearn.datasets import make_classification # 自动生成分类数据集
+from sklearn.model_selection import train_test_split # 生成一个二分类数据集
+from sklearn.linear_model import LogisticRegression # 逻辑回归模型（分类模型）
+
+# 1. 生成数据集
+X, y = make_classification(n_samples=1000, n_features=10, n_classes=2, random_state=42) # 生成一个二分类数据集，包含1000个样本，每个样本有10个特征，随机种子为42
+print(X.shape, y.shape) # (1000, 10) (1000,)
+
+# 2. 划分数据集
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# 3. 定义模型
+model = LogisticRegression()
+
+# 4. 训练模型
+model.fit(x_train, y_train)
+
+# 5. 预测（测试）
+y_pred = model.predict(x_test)
+
+# 6. 生成评估报告
+report = classification_report(y_true=y_test, y_pred=y_pred)
+print(report)
